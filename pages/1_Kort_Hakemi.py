3f21bcf0832491170d80be6b7323dcb8a09e3816
husnukazli <husnukazli@gmail.com>
2026-08-15 21:21:19 +0300

    Add initial Streamlit app for Kort Hakemi

---
+++ pages/1_Kort_Hakemi.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Kort Hakemi", page_icon="📱")
+
+st.title("📱 Kort Hakemi Paneli")
+st.write("Bu ekrandan kort seçimi yapılıp kura ve canlı skor girişi yapılacak.")
+st.warning("Henüz maç programı yüklenmedi.")
